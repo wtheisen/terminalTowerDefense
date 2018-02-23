73f45f223971da11a2db75b4624b2dc12385a138
Alex Brizius <abrizius@nd.edu>
2018-02-22 22:55:57 -0500

    Added placeholder src, include files for enemy

---
+++ include/enemy.h
@@ -0,0 +1,18 @@
+#ifndef enemy_h
+#define enemy_h
+
+#include "logging.h"
+
+struct enemy
+{
+    char type;
+    int x, y;
+    int hp;
+    //int (*shoot)();
+};
+
+typedef struct enemy enemy;
+
+//int shoot(enemy * self);
+
+#endif
---
+++ src/enemy.c
@@ -0,0 +1,24 @@
+#include "enemy.h"
+
+/*int shoot(enemy * self)
+{
+    //x is the direction, as is y. 0,0 is assumed the be the bottom right corner
+
+    switch(self->fireDir)
+    {
+        case NORTH:
+            writeLog("north");
+            break;
+        case SOUTH:
+            writeLog("south");
+            break;
+        case EAST:
+            writeLog("east");
+            break;
+        case WEST:
+            writeLog("west");
+            break;
+    }
+
+    return 0;
+}*/
